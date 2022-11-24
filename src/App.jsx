7691c16e2ff055f7481e8d5461a75919e70c9594
João Vitor Marques Braga <102833436+Joao-Vitor-Marques-Braga@users.noreply.github.com>
2022-11-24 06:19:47 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import './App.css';
 import { Header } from './components/Header/index_header.jsx';
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ProdutosLista } from './components/Produtos/index_produtos';
+import { Administrador } from './pages/Administrador';
 
 function App() {
   return (
     <div className="App">
       <Router>
       <Header/>
-      <ProdutosLista/>
+      <Administrador/>
       </Router>
     </div>
   );
---
+++ src/pages/Administrador/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Header } from '../../components/Header/index_header'
+import './style.css'
+
+export const Administrador = () => {
+  return (
+    <div>
+        <Header/>
+        <div className='informações_do_produto'>
+            <div className='titulo'>
+            <h1>Cadastrar novo produto</h1>
+            </div>
+            <div className='informações_do_produto_final'>
+                <h2>Nome do Produto:</h2>
+                <input type="text" name="nome" id="nome" placeholder='Nome do Produto:'/>
+                <h2>Categoria do produto</h2>
+                <select name="categoria" id="cat">
+                    <option value="">Casual</option>
+                    <option value="">Corrida</option>
+                    <option value="">Chuteira</option>
+                    <option value="">Basquete</option>
+                    <option value="">Treino & Academia</option>
+                </select>
+                <h2>Valor do produto</h2>
+                <input type="text" placeholder='Valor do Produto:'/>
+            </div>
+        </div>
+        <div>
+            <button className='btn-cadastrar'>Cadastrar produto</button>
+        </div>
+    </div>
+  )
+}
